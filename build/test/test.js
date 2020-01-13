81524a69cdd6ccd93007db303b66b8f484302624
kev <kev@localhost>
2020-01-13 16:40:13 +0800

    Auto commit: 2020-01-13_16:40:13

--- build/test/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const src_1 = require("../src");
-test('Can yo', () => {
-    expect(src_1.yo).toBe('yoo');
-});
-//# sourceMappingURL=test.js.map
